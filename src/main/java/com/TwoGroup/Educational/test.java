8190b4fa1c4b1b5144b71dc5a15bf70f3187a621
黄彬朕 <1148049547@qq.com>
2019-05-23 19:39:00 +0800

    搭建项目结构、之后由大家一起编码

---
+++ src/main/java/com/TwoGroup/Educational/test.java
@@ -0,0 +1,13 @@
+package com.TwoGroup.Educational;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class test {
+
+    @GetMapping("/test")
+    public String test(){
+        return "index";
+    }
+}
