711863a2fedb0769f4d7ad09ec519838a0f7c29f
furundong <furundong@qq.com>
2019-05-28 23:29:37 +0800

    增加课程查询功能.

--- src/main/java/com/twoGroup/educational/controller/DevelopmentManageController.java
+++ src/main/java/com/twoGroup/educational/controller/DevelopmentManageController.java
@@ -1,6 +1,7 @@
 package com.twoGroup.educational.controller;
 
-import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.twoGroup.educational.dataTransUtil.DataTransUtil;
@@ -14,85 +15,105 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.Condition;
 
 /**
+ * 课程管理
+ *
  * @Author: 黄彬朕
- * @Date:   19.05.27
+ * @Date: 19.05.27
  */
 @Controller
 @RequestMapping("developmentManage")
 public class DevelopmentManageController {
-    private Logger logger=LoggerFactory.getLogger(DevelopmentManageController.class);
+    private Logger logger = LoggerFactory.getLogger(DevelopmentManageController.class);
 
     @Autowired
     /*课程信息业务接口*/
-    DisciplineInfoService lessonInfoService;
+            DisciplineInfoService lessonInfoService;
 
     //跳转地址前缀
-    private String locationURI="manager/developmentManage";
-    //数据传输工具
-    private DataTransUtil dataTransUtil;
+    private String locationURI = "manager/developmentManage";
 
     //所有课程信息
     private List<DisciplineInfo> lessonInfos;
-    //课程信息
-    private DisciplineInfo lessonInfo;
 
     //作为测试
     /*教务管理下跳转的页面放行*/
     @GetMapping("{managePage}")
-    public String test(@PathVariable String managePage){
-        return locationURI+"/"+managePage;
+    public String test(@PathVariable String managePage) {
+        return locationURI + "/" + managePage;
     }
 
     /**
-     * 课程管理
-     * 分页查询所有课程信息
-    */
-    @GetMapping("info/listLessonManage/{currentPage}")
-    public @ResponseBody String listLessonManage(Map map,@PathVariable int currentPage){
-
-        try {
-            //每页显示五行数据
-            PageHelper.startPage(currentPage,5);
-            //获取数据
-            lessonInfos = lessonInfoService.selectList(null);
-            if (lessonInfos != null) {
-                dataTransUtil=new DataTransUtil();
-                //数据绑定
-                dataTransUtil.dataUtil(map,lessonInfos);
-            }
-        } catch (Exception e) {
-            logger.info("han---获取课程信息失败");
-        }
-        return JSON.toJSONString(map);
-    }
-
-    /**
-     * 课程管理
-     * 根据课程名称模糊查询课程信息
+     * 分页查询所有课程信息,
      */
-    @PostMapping("info/showLessonInfo")
-    public @ResponseBody String showLessonInfo(Map map,String lessonName){
-        if (lessonName==null){
-            listLessonManage(map,1);
-        }else {
-            System.out.println(lessonName);
+    @GetMapping("info/listLessonManage/{currentPage}")
+    public @ResponseBody String listLessonManage(Map map, @PathVariable int currentPage, DisciplineInfo disciplineInfo) {
+        //每页显示五行数据
+        PageHelper.startPage(currentPage, 5);
+        //获取数据
+        lessonInfos = lessonInfoService.selectList(null);
+        if (lessonInfos != null) {
+            //数据绑定
+            return DataTransUtil.dataUtil(map, lessonInfos);
         }
-        return JSON.toJSONString(map);
+        return "error";
     }
 
 
+    /**
+     * 条件查询
+     * @param map
+     * @param currentPage
+     * @param disciplineInfo
+     * @return
+     */
+    @PostMapping("info/listLessonManageLike")
+    public @ResponseBody String listLessonManageLike(Map<String,Object> map,int currentPage , DisciplineInfo disciplineInfo){
+        Wrapper<DisciplineInfo> disciplineName = null;
+        //判断有无条件
+        if (disciplineInfo.getDisciplineName() != null) {
+            if (!"".equals(disciplineInfo.getDisciplineName().trim())) {
+                disciplineName = new EntityWrapper<DisciplineInfo>().like("discipline_name", disciplineInfo.getDisciplineName().trim());
+            }
+        }
+        //每页显示五行数据
+        PageHelper.startPage(currentPage, 5);
+        //获取数据
+        lessonInfos = lessonInfoService.selectList(disciplineName); //也可以用匿名类
+        if (lessonInfos != null) {
+            //数据绑定
+            return DataTransUtil.dataUtil(map, lessonInfos);
+        }
+        return "error";
+    }
+
+    /**
+     * 删除课程信息
+     *
+     * @param discipline_id
+     * @return
+     */
+    @DeleteMapping("info/deleteDiscipline/{discipline_id}")
+    public @ResponseBody
+    String deleteDiscipline(@PathVariable String discipline_id) {
+        boolean b = lessonInfoService.deleteById(Integer.parseInt(discipline_id));
+        if (b == false) {
+            return "fail";
+        }
+        return "true";
+    }
 
     @GetMapping("saveOrUpdate/{page}")
-    public String testModifyData(@PathVariable String page){
-        return locationURI+"/saveOrUpdate/"+page;
+    public String testModifyData(@PathVariable String page) {
+        return locationURI + "/saveOrUpdate/" + page;
     }
 
 
     /*判断执行方法*/
-    public void intoFunction(String managePage,Map map){
-        switch (managePage){
+    public void intoFunction(String managePage, Map map) {
+        switch (managePage) {
             /*课程管理*/
             case "lessonManage":
                 /*查询所有课程信息*/
--- src/main/java/com/twoGroup/educational/dataTransUtil/DataTransUtil.java
+++ src/main/java/com/twoGroup/educational/dataTransUtil/DataTransUtil.java
@@ -1,5 +1,6 @@
 package com.twoGroup.educational.dataTransUtil;
 
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageInfo;
 
 import java.util.HashMap;
@@ -9,12 +10,14 @@ import java.util.Map;
 public class DataTransUtil {
 
     /*封装数据*/
-    public void dataUtil(Map map,List list){
+    public static String  dataUtil(Map map,List list){
         PageInfo pageInfo=new PageInfo(list);
         map.put("status",0);
         map.put("list", list);
         map.put("currentPage",pageInfo.getPageNum());
         map.put("total",pageInfo.getTotal());
         map.put("totalPage",pageInfo.getPages());
+
+        return JSON.toJSONString(map);
     }
 }
