8ea4dd279a207bfc63a44971d0a6bc6918130458
aaa <89114@164.com>
2019-05-31 09:59:12 +0800

    前台首页页面

--- src/main/java/com/twoGroup/educational/controller/TemplateInfoController.java
+++ src/main/java/com/twoGroup/educational/controller/TemplateInfoController.java
@@ -2,8 +2,10 @@ package com.twoGroup.educational.controller;
 
 
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.github.pagehelper.PageHelper;
-import com.twoGroup.educational.dataTransUtil.DataTransUtil;
+import com.twoGroup.educational.hanUtil.DataTransUtil;
 import com.twoGroup.educational.entities.TemplateInfo;
 import com.twoGroup.educational.service.TemplateInfoService;
 import org.slf4j.Logger;
@@ -25,32 +27,26 @@ import java.util.Map;
  * @since 2019-05-26
  */
 @Controller
-@RequestMapping("/InforManager")
+@RequestMapping("InforManager")
 public class TemplateInfoController {
     private Logger logger = LoggerFactory.getLogger(TemplateInfoController.class);
 
     @Autowired
     TemplateInfoService templateInfoService;
 
-    private String locationURI = "manager/InforManager";      //跳转路径前缀
+    private String locationURI = "manager/InformManager";      //跳转路径前缀
 
     private DataTransUtil dataTransUtil;        //数据传输工具
 
     private List<TemplateInfo> templateInfos;
 
-    //直接跳转需要的页面
-    @GetMapping("{redictPage}")
-    public String templateList(@PathVariable String redictPage) {
-        System.out.println("go to " + redictPage);
-        return locationURI + "/" + redictPage;
-    }
+
 
     //获取当前页数据
     @GetMapping("tamplateCurrentList/{currentPage}")
     public @ResponseBody
     String TemplateCurrentList(Map<String, Object> map, @PathVariable Integer currentPage) {
         System.out.println("get TemplateCurrentList!");
-
         PageHelper.startPage(currentPage, 5);        //获取当前页记录,每页5条
         templateInfos = templateInfoService.selectList(null);
         if (templateInfos != null && templateInfos.size() != 0) {
@@ -61,8 +57,24 @@ public class TemplateInfoController {
         return JSON.toJSONString(map);
     }
 
+    @PostMapping("/templateLikeList")
+    public @ResponseBody String  templateLikeList(Map<String,Object> map,String templateContent,Integer currentPage){
+        System.out.println("get TemplateLikeList!"+templateContent+currentPage);
+        Wrapper<TemplateInfo> wrapper =  new EntityWrapper<TemplateInfo>();
+        wrapper.like("template_content",templateContent);
+        PageHelper.startPage(currentPage, 5);
+        templateInfos = templateInfoService.selectList(wrapper);
+
+        if (templateInfos != null && templateInfos.size() != 0) {
+            dataTransUtil = new DataTransUtil();
+            dataTransUtil.dataUtil(map, templateInfos);
+        } else
+            logger.info("查询数据失败");
+        return JSON.toJSONString(map);
+    }
+
     //删除模板信息
-    @DeleteMapping("templateDelete")
+    @DeleteMapping("templateDelete/{templateId}")
     public @ResponseBody  String templateDelete(@PathVariable Integer templateId) {
         logger.info("删除模板信息！");
         try {
